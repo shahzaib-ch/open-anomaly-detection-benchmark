e3ca4ba332ad6e1efd31c813ce5af23c36173d0d
shahzaib <shahzaib.aslam203@gmail.com>
2021-08-29 15:42:55 +0200

    updated timestamp column name

--- play.py
+++ play.py
@@ -3,4 +3,4 @@ from visualizer.visualizing_tools import get_result_data_and_show
 
 import pandas as pd
 
-update_column_name_for_all_file_in_folder("data/datasets/yahoo/A4Benchmark")
+update_column_name_for_all_file_in_folder("data/datasets/yahoo/A3Benchmark", "timestamps", "timestamp")
