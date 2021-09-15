43d1eb3552694534173941396f8722a784d9a0ca
shahzaib <shahzaib.muhammad@shortcut.no>
2021-09-15 23:45:06 +0200

    created result overview dialog

--- play.py
+++ play.py
@@ -1,3 +1,3 @@
-from visualizer.visualizing_tools import show_result_overview_heat_map
+from visualizer.visualizing_tools import show_full_detailed_result_heat_map
 
-show_result_overview_heat_map()
+show_full_detailed_result_heat_map()
