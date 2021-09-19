ef302a7d5b098d6b87314a8a9e57cd43fd2ff7d5
shahzaib <shahzaib.muhammad@shortcut.no>
2021-09-19 20:55:05 +0200

    integrated NAB dataset

--- detector/elliptic_envelope.py
+++ detector/elliptic_envelope.py
@@ -1,14 +1,17 @@
 from abc import ABC
 
+from numpy import log
 from sklearn.covariance import EllipticEnvelope
 
 from detector.base_detector import BaseDetector
-from helper.LabelsHelper import replace_in_array
+from helper.labels_helper import replace_in_array
 
 
 class EllipticEnvelopeDetector(BaseDetector, ABC):
+    __not_supported_datasets = ['data/datasets/nab/artificialNoAnomaly/art_flatline.csv']
+
     def createInstance(self):
-        self.model = EllipticEnvelope(random_state=10)
+        self.model = EllipticEnvelope(random_state=10, support_fraction=1)
 
     def train(self, input_instances, labels):
         self.model.fit(input_instances, labels)
@@ -18,3 +21,6 @@ class EllipticEnvelopeDetector(BaseDetector, ABC):
         labels = replace_in_array(labels, 1, 0)
         labels = replace_in_array(labels, -1, 1)
         return labels
+
+    def notSupportedDatasets(self):
+        return self.__not_supported_datasets
--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -13,12 +13,19 @@ class PreProcessor:
         input_instances_train, input_instances_test, labels_train, labels_test
         """
         df = pd.read_csv(self.file_path)
-        # cleaning unnecessary columns
-        # df.pop("timestamps")
+        # cleaning data
+        df = self.__clean_data(df)
         labels = df["is_anomaly"]
         input_instances = df.loc[:, df.columns != "is_anomaly"]
-        return train_test_split(input_instances, labels, train_size=0.5, shuffle=False)
+        input_instances = input_instances.set_index("timestamp")
+        return input_instances, labels
 
     def get_input_instances_and_labels_split(self):
-        input_instances_train, input_instances_test, labels_train, labels_test = self.__get_data_from_file()
+        input_instances, labels = self.__get_data_from_file()
+        input_instances_train, input_instances_test, labels_train, labels_test = \
+            train_test_split(input_instances, labels, train_size=0.5, shuffle=False)
         return input_instances_train, input_instances_test, labels_train, labels_test
+
+    def __clean_data(self, df):
+        data = df.dropna()
+        return data
