d574132c235bf77ddb86e2900699a6e1864759ba
shahzaib <shahzaib.aslam203@gmail.com>
2021-10-10 23:53:18 +0200

    added another button

--- visualizer/dataset_file_result_summary_window.py
+++ visualizer/dataset_file_result_summary_window.py
@@ -5,9 +5,10 @@ from tkinter.ttk import *
 
 class DatasetResultSummaryWindow:
 
-    def __init__(self, view_dataset_command, view_dataset_with_anomalies_command):
+    def __init__(self, view_dataset_command, view_dataset_labels, visualize_dataset_with_anomalies):
         self.view_dataset_command = view_dataset_command
-        self.view_dataset_with_anomalies_command = view_dataset_with_anomalies_command
+        self.view_dataset_labels_command = view_dataset_labels
+        self.view_dataset_with_anomalies_command = visualize_dataset_with_anomalies
 
     def show_window(self):
         root = tk.Tk()
@@ -24,8 +25,13 @@ class DatasetResultSummaryWindow:
         view_dataset_button = tk.Button(frame, text="View dataset file", command=self.view_dataset_command)
         view_dataset_button.pack(side=TOP, pady=5, ipady=3)
 
+        view_dataset_labels = tk.Button(frame,
+                                        text="View actual labels with detected labels",
+                                        command=self.view_dataset_labels_command)
+        view_dataset_labels.pack(side=TOP, pady=5, ipady=3)
+
         view_dataset_with_anomalies = tk.Button(frame,
-                                                text="View dataset file with detected anomalies",
+                                                text="View dataset with anomalies",
                                                 command=self.view_dataset_with_anomalies_command)
         view_dataset_with_anomalies.pack(side=TOP, pady=5, ipady=3)
 
