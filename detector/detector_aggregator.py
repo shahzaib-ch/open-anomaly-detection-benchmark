733c23b8d6fbe4704ac25dbbcb7fd02bc0e48db7
shahzaib <shahzaib.aslam203@gmail.com>
2021-10-14 23:27:47 +0200

    added knn cad from nab

--- detector/detector_aggregator.py
+++ detector/detector_aggregator.py
@@ -1,9 +1,11 @@
 from detector.elliptic_envelope import EllipticEnvelopeDetector
 from detector.isolation_forest import IsolationForestDetector
+from detector.knncad.knn_cad import KnncadDetector
 from detector.local_outlier_factor import LocalOutlierFactorDetector
 
 ALGORITHMS_DICTIONARY = {
-    "Elliptic Envelope": EllipticEnvelopeDetector(),
-    "Isolation Forest": IsolationForestDetector(),
-    "Local Outlier Factor": LocalOutlierFactorDetector(),
+    # "Elliptic Envelope": EllipticEnvelopeDetector(),
+    # "Isolation Forest": IsolationForestDetector(),
+    # "Local Outlier Factor": LocalOutlierFactorDetector(),
+    "KNN CAD": KnncadDetector(),
 }
