c25a8f0444ad5dc87bf6c92546c2f10271f983eb
shahzaib <shahzaib.aslam203@gmail.com>
2021-10-15 22:58:20 +0200

    added 2 more algorithms

--- detector/detector_aggregator.py
+++ detector/detector_aggregator.py
@@ -1,8 +1,12 @@
+from detector.bayes_change_point import BayesChangePointDetector
 from detector.knn_cad import KnncadDetector
+from detector.windowed_gaussian import WindowedGaussianDetector
 
 ALGORITHMS_DICTIONARY = {
     # "Elliptic Envelope": EllipticEnvelopeDetector(),
     # "Isolation Forest": IsolationForestDetector(),
     # "Local Outlier Factor": LocalOutlierFactorDetector(),
-    "KNN CAD": KnncadDetector(),
+    # "KNN CAD": KnncadDetector(),
+    # "Windowed Gaussian": WindowedGaussianDetector(),
+    "Bayes Change Point": BayesChangePointDetector(),
 }
---
+++ detector/windowed_gaussian.py
@@ -0,0 +1,31 @@
+from abc import ABC
+
+import numpy as np
+
+from detector.base_detector import BaseDetector
+from detector.nab.gaussian.windowedGaussian_detector import WindowedGaussianDetectorNab
+
+
+class WindowedGaussianDetector(BaseDetector, ABC):
+    __not_supported_datasets = []
+    __input_instances_train = []
+    __probationary_period = 0
+
+    def createInstance(self):
+        """
+        we will create instance in predict method due to NAB implementation
+        """
+
+    def train(self, input_instances, labels):
+        self.__input_instances_train = input_instances
+        self.__probationary_period = len(input_instances)
+
+    def predict(self, input_instances):
+        input_instances = np.concatenate((self.__input_instances_train, input_instances))
+        self.model = WindowedGaussianDetectorNab(input_instances, self.__probationary_period)
+        self.model.initialize()
+        labels = self.model.run()
+        return labels
+
+    def notSupportedDatasets(self):
+        return self.__not_supported_datasets
