9ceb25d5b41a9476a38866b5b67589532037f145
shahzaib <shahzaib.muhammad@shortcut.no>
2021-10-19 21:36:10 +0200

    added values for nuerons

--- detector/auto_encoder.py
+++ detector/auto_encoder.py
@@ -8,8 +8,11 @@ from detector.base_detector import BaseDetector
 class AutoEncoderDetector(BaseDetector, ABC):
     __not_supported_datasets = []
 
-    def createInstance(self):
-        self.model = AutoEncoder(hidden_neurons=[1, 1])
+    def createInstance(self, features_count):
+        hidden_neurons = [1, 1]
+        if features_count > 1:
+            hidden_neurons = [features_count * 2/3, features_count, features_count, features_count * 2/3]
+        self.model = AutoEncoder(hidden_neurons=hidden_neurons)
 
     def train(self, input_instances, labels):
         self.model.fit(input_instances)
