aa564f4892a94425b0eb6e8a4abb6062dcd3eb28
shahzaib <shahzaib.aslam203@gmail.com>
2021-10-20 08:54:52 +0200

    added another config in benchmark

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from core.core import do_benchmarking
 
 if __name__ == '__main__':
     # pass desired training dataset size e.g. 0.3 means 30 percent
-    do_benchmarking(training_dataset_size=0.3)
+    do_benchmarking(training_dataset_size=0.3, do_not_update_existing_result=True)
