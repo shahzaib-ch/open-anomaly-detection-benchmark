c3b91ab6a08d7f64226e9750a1cb33d6369198fc
shahzaib <shahzaib.aslam203@gmail.com>
2021-10-26 01:22:53 +0200

    added loc

--- detector/local_outlier_factor.py
+++ detector/local_outlier_factor.py
@@ -10,16 +10,15 @@ class LocalOutlierFactorDetector(BaseDetector, ABC):
     __not_supported_datasets = []
 
     def createInstance(self, features_count, contamination):
-        self.model = LocalOutlierFactor(contamination=contamination)
+        self.model = LocalOutlierFactor(contamination=contamination, novelty=True)
 
     def train(self, input_instances, labels):
         self.model.fit(input_instances, labels)
 
     def predict(self, input_instances):
-        labels = self.model.predict(input_instances)
-        labels = replace_in_array(labels, 1, 0)
-        labels = replace_in_array(labels, -1, 1)
-        return labels
+        scores = self.model.decision_function(input_instances)
+        scores = 1 - scores
+        return scores
 
     def notSupportedDatasets(self):
         return self.__not_supported_datasets
