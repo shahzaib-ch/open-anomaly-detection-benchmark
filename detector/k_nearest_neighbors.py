1f68aca80c97a5ede9322aa4b9245ae143240d38
shahzaib <shahzaib.muhammad@shortcut.no>
2021-10-27 23:06:22 +0200

    fixed pyod scoring issues

--- detector/k_nearest_neighbors.py
+++ detector/k_nearest_neighbors.py
@@ -3,6 +3,7 @@ from abc import ABC
 from pyod.models.knn import KNN
 
 from detector.base_detector import BaseDetector
+from helper.common_methods import get_2nd_value_from_list
 
 
 class KNearestNeighborsDetector(BaseDetector, ABC):
@@ -15,7 +16,8 @@ class KNearestNeighborsDetector(BaseDetector, ABC):
         self.model.fit(input_instances)
 
     def predict(self, input_instances):
-        scores = self.model.decision_function(input_instances)
+        scores = self.model.predict_proba(input_instances)
+        scores = get_2nd_value_from_list(scores)
         return scores
 
     def notSupportedDatasets(self):
