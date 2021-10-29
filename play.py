c6a24dc5a3914f9082dc57e9eb9b22ec0638db4c
shahzaib <shahzaib.aslam203@gmail.com>
2021-10-29 12:36:55 +0200

    some improvements

--- play.py
+++ play.py
@@ -1,7 +1,11 @@
-from helper.ResultWindowLabeler import ResultWindowLabeler
+import pandas as pd
+from sklearn.metrics import confusion_matrix
 
-a = [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
-d = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
-labeler = ResultWindowLabeler(a, d)
-result = labeler.mark_whole_window_if_any_point_detected()
-print(result)
+labels = [0, 1, 0, 1, 1, 0, 0, 0]
+scores = [0, 0, 0, 1, 1, 0, 1, 0]
+
+sf = pd.DataFrame(scores, index="daa")
+sf["hgsd"] = labels
+
+
+print(sf.isna())
