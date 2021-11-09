1b8f393a31804467bce0cd37437e3d673b4d78c3
shahzaib <shahzaib.aslam203@gmail.com>
2021-11-09 20:59:50 +0100

    fixed average precision crash

--- result_accuracy_visualizer.py
+++ result_accuracy_visualizer.py
@@ -1,5 +1,5 @@
 from visualizer.result_data_keys import ResultDataKey
 from visualizer.result_accuracy_visualizer import AccuracyResultVisualizer
 
-visualizer = AccuracyResultVisualizer(accuracy_measure=ResultDataKey.precision, anomaly_threshold=0.90, use_windows=True)
+visualizer = AccuracyResultVisualizer(accuracy_measure=ResultDataKey.average_precision_score, anomaly_threshold=0.90, use_windows=True)
 visualizer.show_result_overview_heat_map()
